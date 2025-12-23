ee1f0eed61eb11d7cc53ee23932cd9fe91850693
nii-gakunin-cloud <40594865+nii-gakunin-cloud@users.noreply.github.com>
2025-12-23 15:02:15 +0900

    jupyter_server.serverappに移行

--- CoursewareHub/scripts/edit_conf.py
+++ CoursewareHub/scripts/edit_conf.py
@@ -1,18 +1,14 @@
 from pathlib import Path
 
 from IPython.core.display import HTML
-from notebook.notebookapp import list_running_servers
+from jupyter_server.serverapp import list_running_servers
 
 
 def generate_edit_link(conf: Path) -> HTML:
     servers = list(list_running_servers())
     if len(servers) > 0:
         nb_conf = servers[0]
-        p = (
-            Path(nb_conf["base_url"])
-            / "edit"
-            / conf.absolute().relative_to(nb_conf["notebook_dir"])
-        )
+        p = Path(nb_conf["base_url"]) / "edit" / conf.absolute().relative_to(nb_conf["root_dir"])
     else:
         p = conf.absolute()
     return HTML(f'<a href={p} target="_blank">{p.name}</a>')
--- OpenHPC-v3/scripts/edit_conf.py
+++ OpenHPC-v3/scripts/edit_conf.py
@@ -1,20 +1,19 @@
-from datetime import datetime
-from difflib import unified_diff
-from logging import basicConfig, getLogger, INFO
-from pathlib import Path
 import shutil
-from subprocess import run, PIPE
 import sys
+from datetime import datetime, timezone
+from difflib import unified_diff
+from logging import INFO, basicConfig, getLogger
+from pathlib import Path
+from subprocess import PIPE, run
+
 import yaml
-
-from notebook import notebookapp
 from IPython.core.display import HTML
+from jupyter_server import serverapp
 
-
-WORKDIR = 'edit'
-META_YML = '.vcp-meta.yml'
+WORKDIR = "edit"
+META_YML = ".vcp-meta.yml"
 logger = getLogger(__name__)
-basicConfig(level=INFO, format='%(message)s')
+basicConfig(level=INFO, format="%(message)s")
 
 
 def fetch_conf(target_path, ugroup, unit):
@@ -36,38 +35,44 @@ def upload_conf(target_path, ugroup, unit=None, version=None):
 
 def download_conf_file(target_path, ugroup, unit):
     dest = generate_local_path(target_path, ugroup)
-    ansible_arg = f'src={target_path} dest={dest} flat=yes'
-    group = f'{ugroup}_{unit}'
-    args = ['ansible', group, '-b', '-m', 'fetch', '-a', ansible_arg]
-    output = run(args, check=True, stdout=PIPE, text=True,).stdout
-    host_1 = output.split("\n")[0].split()[0]
+    ansible_arg = f"src={target_path} dest={dest} flat=yes"
+    group = f"{ugroup}_{unit}"
+    args = ["ansible", group, "-b", "-m", "fetch", "-a", ansible_arg]
+    output = run(
+        args,
+        check=True,
+        stdout=PIPE,
+        text=True,
+    ).stdout
+    host_1 = output.split("\n", maxsplit=1)[0].split()[0]
 
-    logger.info(f'Downloading {target_path} from {host_1} to {dest}')
+    logger.info("Downloading %s from %s to %s", target_path, host_1, dest)
     make_metainfo(dest, target_path, unit, host_1)
     return dest
 
 
 def _output_to_hosts(output):
-    return ",".join(sorted([
-        line.split()[0] for line in output.split("\n")
-        if line.find('CHANGED =>') >= 0
-    ]))
+    return ",".join(sorted([line.split()[0] for line in output.split("\n") if line.find("CHANGED =>") >= 0]))
 
 
 def upload_conf_file(local_path, target_path, ugroup, unit=None):
-    ansible_arg = (
-        f'dest={target_path} src={local_path} owner=no group=no perms=no')
-    group = f'{ugroup}_{unit}' if unit is not None else ugroup
-    args = ['ansible', group, '-b', '-m', 'synchronize', '-a', ansible_arg]
-    output = run(args, check=True, stdout=PIPE, text=True,).stdout
+    ansible_arg = f"dest={target_path} src={local_path} owner=no group=no perms=no"
+    group = f"{ugroup}_{unit}" if unit is not None else ugroup
+    args = ["ansible", group, "-b", "-m", "synchronize", "-a", ansible_arg]
+    output = run(
+        args,
+        check=True,
+        stdout=PIPE,
+        text=True,
+    ).stdout
     hosts = _output_to_hosts(output)
-    logger.info(f'Uploading {target_path} from {local_path} to {hosts}')
+    logger.info("Uploading %s from %s to %s", target_path, local_path, hosts)
 
 
 def generate_local_path(target_path, ugroup, version=None):
     ret = Path(WORKDIR).absolute() / ugroup
     if version is None:
-        ret /= datetime.now().strftime("%Y%m%d%H%M%S%f")
+        ret /= datetime.now(tz=timezone.utc).strftime("%Y%m%d%H%M%S%f")
     else:
         ret /= version
     ret /= Path(target_path).name
@@ -76,32 +81,31 @@ def generate_local_path(target_path, ugroup, version=None):
 
 def make_metainfo(local_path, target_path, unit, host):
     params = {
-        'path': target_path,
-        'unit': unit,
-        'host': host,
-        'version': list(local_path.parts)[-2],
+        "path": target_path,
+        "unit": unit,
+        "host": host,
+        "version": list(local_path.parts)[-2],
     }
-    with (local_path.parent / META_YML).open(mode='w') as f:
+    with (local_path.parent / META_YML).open(mode="w") as f:
         yaml.safe_dump(params, stream=f, default_flow_style=False)
 
 
 def make_backup(conf):
     org = _to_backup(conf)
-    logger.info(f'Copy {conf} {org}')
+    logger.info("Copy %s to %s", conf, org)
     shutil.copy2(conf, org)
 
 
 def _to_backup(conf):
-    return conf.parent / (conf.name + '.orig')
+    return conf.parent / (conf.name + ".orig")
 
 
 def generate_edit_link(conf):
-    servers = list(notebookapp.list_running_servers())
+    servers = list(serverapp.list_running_servers())
     if len(servers) == 0:
         return str(conf.absolute())
     nb_conf = servers[0]
-    p = (Path(nb_conf['base_url']) / 'edit' /
-         conf.absolute().relative_to(nb_conf['notebook_dir']))
+    p = Path(nb_conf["base_url"]) / "edit" / conf.absolute().relative_to(nb_conf["root_dir"])
     return HTML(f'<a href={p} target="_blank">{p.name}</a>')
 
 
@@ -117,9 +121,7 @@ def find_latest_version(target_path, ugroup, unit=None):
 
 def get_versions(target_path, ugroup, unit=None):
     pdir = Path(WORKDIR).absolute() / ugroup
-    return sorted([
-        x.name for x in pdir.glob('*')
-        if x.is_dir() and _match_metainfo(x, target_path, unit)])
+    return sorted([x.name for x in pdir.glob("*") if x.is_dir() and _match_metainfo(x, target_path, unit)])
 
 
 def _match_metainfo(parent, target_path, unit=None):
@@ -129,11 +131,10 @@ def _match_metainfo(parent, target_path, unit=None):
     with p.open() as f:
         params = yaml.safe_load(f)
     return (
-        isinstance(params, dict) and
-        'path' in params and
-        params['path'] == target_path and (
-            unit is None or ('unit' in params and params['unit'] == unit)
-        )
+        isinstance(params, dict)
+        and "path" in params
+        and params["path"] == target_path
+        and (unit is None or ("unit" in params and params["unit"] == unit))
     )
 
 
@@ -144,7 +145,6 @@ def show_diff(path_a, path_b):
         lines_a = f.readlines()
     with path_b.open() as f:
         lines_b = f.readlines()
-    diff = list(unified_diff(
-        lines_a, lines_b, fromfile=path_a.name, tofile=path_b.name))
+    diff = list(unified_diff(lines_a, lines_b, fromfile=path_a.name, tofile=path_b.name))
     sys.stdout.writelines(diff)
     return len(diff)
